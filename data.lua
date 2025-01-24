4e8d7ac5b5c7956a1b8c2a020c8fe72ad06be849
rjdunlap <rjdunlap@outlook.com>
2025-01-24 11:12:32 -0800

    0.0.6 fix pollution

--- data.lua
+++ data.lua
@@ -80,7 +80,8 @@ local akularis=
     },
     map_gen_settings = MapGen_Akularis(),
     asteroid_spawn_influence = 1,
-    asteroid_spawn_definitions = start_astroid_spawn
+    asteroid_spawn_definitions = start_astroid_spawn,
+    pollutant_type = "pollution"
 }
 
 akularis.orbit = {
